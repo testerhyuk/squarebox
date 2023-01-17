7c245a7be5fea75eb5883431f2bf6f22e6de159e
Hyuk <testerhyuk214@gmail.com>
2023-01-17 18:07:22 +0900

    videos

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 import requests from "../api/request";
 import React from 'react';
 import './Banner.css';
+import styled from "styled-components";
 
 export default function Banner() {
     const [movie, setMovie] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
 
     useEffect(()=> {
         fetchData();
@@ -29,33 +31,85 @@ export default function Banner() {
         return str?.length > n ? str.substr(0, n-1) + '...' : str;
     };
 
-  return (
-    <header
-        className="banner"
-        style={{
-            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
-            backgroundPosition: 'top center',
-            backgroundSize: 'cover'
-        }}
-    >
-        <div className="banner_contents">
-            <h1 className="banner_title">
-                {movie?.title || movie?.name || movie?.original_name}
-            </h1>
-            <div className="banner_buttons">
-                <button className="banner_button play">
-                    재생
-                </button>
-                <button className="banner_button info">
-                    <div className="space"></div>
-                    세부정보 더보기
-                </button>
-            </div>
-            <h1 className="banner_description">
-                {truncate(movie?.overview, 100)}
-            </h1>
-        </div>
-        <div className="banner--fadeBottom" />
-    </header>
-  )
+    if (!isClicked) {
+        return (
+            <header
+                className="banner"
+                style={{
+                    backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+                    backgroundPosition: 'top center',
+                    backgroundSize: 'cover'
+                }}
+            >
+                <div className="banner_contents">
+                    <h1 className="banner_title">
+                        {movie?.title || movie?.name || movie?.original_name}
+                    </h1>
+                    <div className="banner_buttons">
+                        <button 
+                            className="banner_button play"
+                            onClick={() => setIsClicked(true)}
+                        >
+                            재생
+                        </button>
+                        <button className="banner_button info">
+                            <div className="space"></div>
+                            세부정보 더보기
+                        </button>
+                    </div>
+                    <h1 className="banner_description">
+                        {truncate(movie?.overview, 100)}
+                    </h1>
+                </div>
+                <div className="banner--fadeBottom" />
+            </header>
+          )
+    } else{
+        return(
+            <Container>
+                <HomeContainer>
+                    <Iframe
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}
+                        ?controls=0&autoplay=1&loop=1&mute=0&playlist=${movie.videos.results[0].key}`}
+                        width='640'
+                        height='360'
+                        allow="autoplay; fullscreen"
+                        frameborder='0'
+                        allowFullScreen
+                    ></Iframe>
+                </HomeContainer>
+            </Container>
+        )
+    }
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-itmes: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`
+
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.65;
+    border: none;
+
+    &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`
